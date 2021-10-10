6dcd31caf3c80db27de7169cf048d538721a8169
Jason Dunn <dunnjm814@gmail.com>
2021-10-10 15:09:03 -0700

    update favicon, and html meta data

--- websynth/src/App.js
+++ websynth/src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
         <Sequencer />
+        <div id='description'>
+          <p>
+            Click anywhere on the grid to draw a sequence. <br/>
+            Pick a musical scale from the key drop-down. <br/>
+            Adjust the playback speed with the tempo slider. <br/>
+            Press play to make some music! <br/>
+          </p>
+        </div>
     </div>
   );
 }
